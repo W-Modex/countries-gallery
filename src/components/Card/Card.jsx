ba4159a47ec1a4e0d0954e718b32e8adec8d82ff
Modex <rifai.ali259@gmail.com>
2024-02-03 19:52:47 +0100

    quick fix

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -4,7 +4,7 @@ function Card({img, alt, name, population, region, capital}) {
   return (
     <div className='mb-5 pb-5 mt-10 shadow-lg bg-white flex flex-col w-[300px] h-[400px] rounded-xl dark:bg-darkElem dark:text-white cursor-pointer'>
       <div className='mb-4'>
-        <img src={img} alt={alt} className='rounded-t-xl h-[200px] ' />
+        <img src={img} alt={alt} className='rounded-t-xl min-h-[200px] h-fit' />
       </div>
       <div className='pl-5 pb-5'>
         <h4 className='text-xl font-extrabold mb-4'>{name}</h4>
